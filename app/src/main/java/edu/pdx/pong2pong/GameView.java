1dc579dab3a54b59f35ef44ba17e2b085051ad93
Josef Mihalits <jm.public@gmail.com>
2015-11-21 12:17:06 -0800

    Add a basic version of onTouchEvent.

--- app/src/main/java/edu/pdx/pong2pong/GameView.java
+++ app/src/main/java/edu/pdx/pong2pong/GameView.java
@@ -6,15 +6,36 @@ package edu.pdx.pong2pong;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 
-public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+public class GameView extends SurfaceView
+        implements SurfaceHolder.Callback {
+
+    /** ball velocity x */
+    float mVx;
+    /** ball velocity y */
+    float mVy;
+
+    float[] mX = new float[3];
+    float[] mY = new float[3];
+
+    /** the width of the screen (max x) */
+    int mScreenW;
+
+    /** the height of the screen (max y) */
+    int mScreenH;
 
     /** The thread that actually draws the animation */
     private final GameThread mThread;
+
+    //private PointF paddle[] = {new PointF(10,10), new PointF(200, 10)};
 
     public GameView(Context context) {
         this(context, null);
@@ -39,14 +60,32 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         setFocusable(true); // make sure we get key events
     }
 
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mScreenW = w;
+        mScreenH = h;
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        mX[0] = mY[0] = 10;
+        mVx = 20;
+        mVy = 10;
+        mX[1] = 5;
+        mY[1] = mScreenH / 2;
+        mX[2] = mScreenW - 30;
+        mY[2] = mScreenH / 2;
+
         mThread.start();
+        Log.d("JM", "surfaceCreated");
+
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+        //Log.d("JM", "surfaceChanged" + width + " " + height);
     }
 
     @Override
@@ -59,15 +98,32 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         }
     }
 
+    /**
+     * handle events: MotionEvent.ACTION_DOWN, MotionEvent.ACTION_UP, MotionEvent.ACTION_MOVE
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //super.onTouchEvent(event);
+
+        float x = event.getX();
+        float y = event.getY();
+        if (x < mScreenW / 2) {
+            //left paddle
+            mY[1] = y;
+        } else {
+            //right paddle
+            mY[2] = y;
+        }
+        return true;
+    }
+
 
     /**
      *
      */
     class GameThread extends Thread {
-        private float mX = 0;
-        private float mY = 0;
-
-        private boolean mInBackground = false;
 
         /** Handle to the surface manager object we interact with */
         private SurfaceHolder mHolder;
@@ -93,16 +149,20 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
                 Canvas c = mHolder.lockCanvas();
                 doDraw(c);
                 mHolder.unlockCanvasAndPost(c);
-                mX += 10;
-                mY += 10;
+                mX[0] += mVx;
+                mY[0] += mVy;
             }
         }
 
 
         private void doDraw (Canvas c) {
+            Paint p = new Paint();
+
             c.drawARGB(255, 150,150,0); //background
             //c.drawBitmap(ball, x - ball.getWidth()/2, y - ball.getHeight()/2, null);
-            c.drawCircle(mX, mY, 10, new Paint());
+            c.drawCircle(mX[0], mY[0], 10, p);
+            c.drawRect(mX[1], mY[1], mX[1]+20, mY[1]+200, p);
+            c.drawRect(mX[2], mY[2], mX[2]+20, mY[2]+200, p);
         }
     }
 }
