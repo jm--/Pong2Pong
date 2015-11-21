ae5d9b5bea454723d58bf9a98fbb498a00da4210
Josef Mihalits <jm.public@gmail.com>
2015-11-21 01:26:06 -0800

    Set landscape and fullscreen.

--- app/src/main/java/edu/pdx/pong2pong/GameActivity.java
+++ app/src/main/java/edu/pdx/pong2pong/GameActivity.java
@@ -2,6 +2,8 @@ package edu.pdx.pong2pong;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,7 +11,9 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         mGameView = new GameView(this);
         setContentView(mGameView);
      }
