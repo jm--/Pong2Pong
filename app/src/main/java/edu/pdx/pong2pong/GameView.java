632f9775275cd8b11baeafdeb623645d2e4366e0
Josef Mihalits <jm.public@gmail.com>
2015-11-23 11:44:32 -0800

    Change "extends Thread" to "implements Runnable".

--- app/src/main/java/edu/pdx/pong2pong/GameView.java
+++ app/src/main/java/edu/pdx/pong2pong/GameView.java
@@ -1,6 +1,8 @@
 package edu.pdx.pong2pong;
 /**
- * Uses code snippets from the Android SDK LunarLander sample program
+ * Uses code snippets from the Android SDK LunarLander sample program:
+ * http://grepcode.com/file/repository.grepcode.com/java/ext/com.google.android/android-apps/2.3.1_r1/com/example/android/lunarlander/LunarView.java
+ *
  */
 
 import android.content.Context;
@@ -9,12 +11,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView
-        implements SurfaceHolder.Callback {
+        implements SurfaceHolder.Callback, Runnable {
 
     Ball mBall;
     Paddle[] mPaddle;
@@ -26,9 +29,22 @@ public class GameView extends SurfaceView
     int mScreenH;
 
     /** The thread that actually draws the animation */
-    private final GameThread mThread;
+    private Thread mThread;
 
+    /** Style and color information for text output */
     private Paint paintText = new Paint();
+
+    /** Indicate whether the surface has been created & is ready to draw */
+    private boolean mRun = false;
+
+    /** Lock to sync access to mRun */
+    private final Object mRunLock = new Object();
+
+    /** Handle to the surface manager object we interact with */
+    private SurfaceHolder mHolder;
+
+    /** time (ms) between frames; one iteration of the main processing loop in run() */
+    private int mDt;
 
     public GameView(Context context) {
         this(context, null);
@@ -38,17 +54,8 @@ public class GameView extends SurfaceView
         super(context, attrs);
 
         // register our interest in hearing about changes to our surface
-        final SurfaceHolder holder = getHolder();
-        holder.addCallback(this);
-
-        // create thread only; it's started in surfaceCreated()
-        mThread = new GameThread(holder, context, new Handler() {
-//            @Override
-//            public void handleMessage(final Message m) {
-//                mStatusText.setVisibility(m.getData().getInt("viz"));
-//                mStatusText.setText(m.getData().getString("text"));
-//            }
-        });
+        mHolder = getHolder();
+        mHolder.addCallback(this);
 
         setFocusable(true); // make sure we get key events
 
@@ -57,7 +64,6 @@ public class GameView extends SurfaceView
         paintText.setTextSize(30);
     }
 
-
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
@@ -65,31 +71,47 @@ public class GameView extends SurfaceView
         mScreenH = h;
     }
 
+    /**
+     * Callback invoked when the Surface has been created and is ready to be
+     * used.
+     */
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
         mBall = new Ball(mScreenW, mScreenH);
         mPaddle = new Paddle[] {
                 new Paddle(20, mScreenH / 2),
                 new Paddle(mScreenW - 20, mScreenH / 2)
         };
-
+        mRun = true;
+        mThread = new Thread(this);
         mThread.start();
     }
 
+    /**
+     * Callback invoked when the surface dimensions change.
+     */
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
     }
 
+    /**
+     * Callback invoked when the Surface has been destroyed and must no longer
+     * be touched. WARNING: after this method returns, the Surface/Canvas must
+     * never be touched again!
+     */
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        // tell thread to shut down
+        mRun = false;
+        // wait for it to finish
         while(true) {
             try {
                 mThread.join();
-                return;
+                break;
             } catch(InterruptedException e) {}
         }
+        mThread = null;
     }
 
     /**
@@ -113,71 +135,43 @@ public class GameView extends SurfaceView
         return true;
     }
 
+    @Override
+    public void run() {
+        long timeEnd = System.currentTimeMillis();
+        while (mRun) {
+            long timeStart = System.currentTimeMillis();
+            //time between frames; adding 1 guarantees that value is never 0
+            mDt = (int) (timeStart - timeEnd + 1);
+            timeEnd = timeStart;
 
-
-    /**
-     *
-     */
-    class GameThread extends Thread {
-
-        /** Handle to the surface manager object we interact with */
-        private SurfaceHolder mHolder;
-
-        /** Message handler used by thread to interact with TextView */
-        private Handler mHandler;
-
-        /** Handle to the application context, used to e.g. fetch Drawables. */
-        private Context mContext;
-
-        /** time (ms) between frames; one iteration of the main processing loop in run() */
-        private int mDt;
-
-
-
-        public GameThread(final SurfaceHolder holder,
-                           final Context context,
-                           final Handler handler) {
-            // get handles to some important objects
-            mHolder = holder;
-            mContext = context;
-            mHandler = handler;
-
-
-        }
-
-        @Override
-        public void run() {
-            long timeEnd = System.currentTimeMillis();
-            while (true) {
-                long timeStart = System.currentTimeMillis();
-                //time between frames; adding 1 guarantees that value is never 0
-                mDt = (int) (timeStart - timeEnd + 1);
-                timeEnd = timeStart;
-
-                Canvas c = mHolder.lockCanvas();
+            Canvas c = mHolder.lockCanvas();
+            if (c != null) {
                 doDraw(c);
                 mHolder.unlockCanvasAndPost(c);
-                mBall.move(mPaddle[0], mPaddle[1], mDt);
+            }
 
-                if (mBall.getX() < 0 || mBall.getX() > mScreenW) {
-                    //ball is outside game area - create a new ball/game
-                    mBall.start();
-                }
+            mBall.move(mPaddle[0], mPaddle[1], mDt);
 
+            if (mBall.getX() < 0 || mBall.getX() > mScreenW) {
+                //ball is outside game area - create a new ball/game
+                mBall.start();
             }
         }
+    }
 
-
-        private void doDraw(Canvas c) {
-            c.drawColor(Color.LTGRAY); //background
-            c.drawText("time between frames (ms): " + mDt, 10, 60, paintText);
-            int fps = (int) (1000 / mDt);
-            c.drawText("frames per second: " + fps, 10, 100, paintText);
-            c.drawText("screen: " + mScreenW + "x" + mScreenH, 10, 140, paintText);
-            c.drawText("speed of ball: " + mBall.mSpeed, 10, 180, paintText);
-            mBall.draw(c);
-            mPaddle[0].draw(c);
-            mPaddle[1].draw(c);
-        }
+    /**
+     * Draw ball, paddles, and everything else to canvas.
+     * @param c canvas
+     */
+    private void doDraw(Canvas c) {
+        c.drawColor(Color.LTGRAY); //background
+        c.drawText("time between frames (ms): " + mDt, 10, 60, paintText);
+        int fps = 1000 / mDt;
+        c.drawText("frames per second: " + fps, 10, 100, paintText);
+        c.drawText("screen: " + mScreenW + "x" + mScreenH, 10, 140, paintText);
+        c.drawText("speed of ball: " + mBall.mSpeed, 10, 180, paintText);
+        mBall.draw(c);
+        mPaddle[0].draw(c);
+        mPaddle[1].draw(c);
     }
 }
